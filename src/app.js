cd513ae6ec3e9bf57dc8d2494d8ffe4e4d0a75ed
Satyam <kumarsatyam01303@gmail.com>
2026-03-02 12:00:38 +0530

    Wire identify route to app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const identifyRoute = require("./routes/identify");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/identify", identifyRoute);
 
 app.get("/", (req, res) => {
   res.send("Bitespeed Identity Reconciliation API");
